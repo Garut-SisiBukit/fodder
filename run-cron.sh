b04b5783fbab262fa760074f742044b0cedc7d24
Garut-SisiBukit <aditiagilang345@gmail.com>
2024-07-17 21:06:27 +0700

    Update run-cron.sh

--- run-cron.sh
+++ run-cron.sh
@@ -14,11 +14,6 @@ echo " Memasang limit ssh ke crontab "
 # // Jalankan pungsi Limit-ssh setiap 1 menit
 echo "*/1 * * * *  root /luna/run/limit-ssh" >>/etc/crontab
 sleep 1
-echo " Memasang xp ke crontab "
-# // Delete Account Trial Yang Sudah Expired Setiap 30 Menit
-echo "*/30 * * * *  root /usr/local/sbin/xp" >>/etc/crontab
-echo "*/1 * * * * root /usr/local/sbin/delexp" >>/etc/crontab
-sleep 1
 echo " Memasang Autokill ssh ke crontab "
 echo "*/1 * * * *  root /luna/run/kill-ssh $iplimit" >>/etc/cron.d
 sleep 1
@@ -38,28 +33,15 @@ echo "\e[92;1m Memasang limit ssh ke cron.d \e[0m"
 # // Jalankan pungsi Limit-ssh setiap 1 menit
 echo "*/1 * * * *  root /luna/run/limit-ssh" >>/etc/cron.d/limit-ssh
 sleep 1
-echo "\e[92;1m Memasang xp ke cron.d \e[0m"
-# // Delete Account Trial Yang Sudah Expired Setiap 30 Menit
-echo "*/1 * * * *  root /usr/local/sbin/xp" >>/etc/cron.d/xp
-sleep 1
 echo "\e[92;1m Memasang autokill ssh ke cron.d \e[0m"
 echo "*/1 * * * *  root /luna/run/kill-ssh" >>/etc/cron.d/kill-ssh
-echo "*/1 * * * * root /usr/local/sbin/delexp" >>/etc/cron.d/delexp
-sleep 1
-#echo " Memasang autolock xray ke cron.d "
-
-#echo "*/1 * * * *  root /usr/local/sbin/lockedvme" >>/etc/cron.d/lockvm
-#echo "*/1 * * * *  root /usr/local/sbin/lockedvle" >>/etc/cron.d/lockvl
-#echo "*/1 * * * *  root /usr/local/sbin/lockedssr" >>/etc/cron.d/lockss
-#echo "*/1 * * * *  root /usr/local/sbin/lockedtro" >>/etc/cron.d/locktr
-#sleep 1
 echo " Memasang autokill xray ke cron.d "
 echo "*/1 * * * * root /usr/local/sbin/killSS" >> /etc/cron.d/autokil-ssr
 echo "*/1 * * * * root /usr/local/sbin/killTR" >> /etc/cron.d/autokil-tro
 echo "*/1 * * * * root /usr/local/sbin/killVL" >> /etc/cron.d/autokil-vle
 echo "*/1 * * * * root /usr/local/sbin/killVM" >> /etc/cron.d/autokil-vme
 echo " memasang Auto delete account "
-echo "*/30 * * * * root /usr/local/sbin/xp-all" >> /etc/cron.d/autodeleteXP
+echo "*/50 * * * * root /usr/local/sbin/xp-all" >> /etc/cron.d/autodeleteXP
 sleep 1
 systemctl daemon-reload
 sleep 2
@@ -69,7 +51,6 @@ echo -e "\e[93;1mIzinkan Service \e[0m"
 systemctl enable xray
 systemctl enable nginx
 systemctl enable udp-mini
-systemctl enable limit-ssh
 systemctl enable limit
 systemctl enable cron
 systemctl enable noobzvpns
@@ -77,8 +58,6 @@ echo -e "\e[93;1mJalankan Service \e[0m"
 # // Menjalankan Service
 systemctl restart xray
 systemctl restart nginx
-systemctl restart limit-ssh
-systemctl restart udp-mini
 systemctl restart cron
 systemctl restart noobzvpns
 clear
